454cd879704c56b8c799eeb06db93404be82edbc
c-chee <christinechee2000@gmail.com>
2026-03-02 12:22:10 -0500

    Revert "Added FAQ section and route.js (#10)" (#12)
    This reverts commit b5baadbfa1f5400e1344df9d346d78f255624814.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 import Hero from '@/components/sections/Hero';
-import FAQPage from '@/components/sections/Faq';
-// import UseLrnrPageCards from '@/components/sections/Cards';
 
 export default function Home() {
   return (
     <div className='flex flex-col w-screen min-h-screen items-center justify-start'>
       <Hero />
-      <FAQPage />
     </div>
   );
 }
