6f52d70d0625931b35b136467eef0012350233aa
c-chee <christinechee2000@gmail.com>
2026-03-02 11:20:23 -0500

    Navigations (#9)
    * build:
    
    Build next project folders and files
    
    * Build:
    
    Build env example file
    
    * Build:
    
    Build color palette
    
    * Feat:
    
    Add lrnr title and turtle images
    
    * Feat:
    
    Add pill button component and top navigations bar
    
    * Feat:
    
    Add footer component
    
    * Navigations (#3)
    
    * build:
    
    Build next project folders and files
    
    * Build:
    
    Build env example file
    
    * Build:
    
    Build color palette
    
    * Feat:
    
    Add lrnr title and turtle images
    
    * Feat:
    
    Add pill button component and top navigations bar
    
    * Feat: (#4)
    
    Add custom 404 page and add error code component
    
    * Feat:
    
    Update branch

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className='relative overflow-hidden w-full px-6 md:px-15 lg:px-18 py-12 lg:pb-10 transition-colors duration-300 bg-[var(--footer-bg)] text-[var(--background)]'>
 
             {/* === Grid container === */}
-            <div className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
+            <div className='mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
 
                 {/* [ Left column ] */}
                 <div className='flex flex-col space-y-6'>
